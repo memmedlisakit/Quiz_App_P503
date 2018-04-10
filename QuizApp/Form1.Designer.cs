92c458becea69a8db2d5f6a2d1400d6c4bdde0b7
memmedlisakit <memmedlisakit@gmail.com>
2018-04-10 21:24:20 +0400

    Quiz application for p503

---
+++ QuizApp/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace QuizApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStuName = new System.Windows.Forms.TextBox();
+            this.btnAddStu = new System.Windows.Forms.Button();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDataStudent = new System.Windows.Forms.TextBox();
+            this.txtDataTask = new System.Windows.Forms.TextBox();
+            this.txtQuiz = new System.Windows.Forms.TextBox();
+            this.btnQuiz = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(102, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Name:";
+            // 
+            // txtStuName
+            // 
+            this.txtStuName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStuName.Location = new System.Drawing.Point(210, 12);
+            this.txtStuName.Name = "txtStuName";
+            this.txtStuName.Size = new System.Drawing.Size(160, 26);
+            this.txtStuName.TabIndex = 1;
+            // 
+            // btnAddStu
+            // 
+            this.btnAddStu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddStu.Location = new System.Drawing.Point(272, 65);
+            this.btnAddStu.Name = "btnAddStu";
+            this.btnAddStu.Size = new System.Drawing.Size(98, 35);
+            this.btnAddStu.TabIndex = 2;
+            this.btnAddStu.Text = "Add Student";
+            this.btnAddStu.UseVisualStyleBackColor = true;
+            this.btnAddStu.Click += new System.EventHandler(this.btnAddStu_Click);
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddTask.Location = new System.Drawing.Point(711, 65);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(98, 35);
+            this.btnAddTask.TabIndex = 5;
+            this.btnAddTask.Text = "Add Task";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // txtTask
+            // 
+            this.txtTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTask.Location = new System.Drawing.Point(649, 12);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(160, 26);
+            this.txtTask.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(541, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Task Name:";
+            // 
+            // txtDataStudent
+            // 
+            this.txtDataStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDataStudent.Location = new System.Drawing.Point(15, 117);
+            this.txtDataStudent.Multiline = true;
+            this.txtDataStudent.Name = "txtDataStudent";
+            this.txtDataStudent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDataStudent.Size = new System.Drawing.Size(355, 82);
+            this.txtDataStudent.TabIndex = 6;
+            // 
+            // txtDataTask
+            // 
+            this.txtDataTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDataTask.Location = new System.Drawing.Point(454, 117);
+            this.txtDataTask.Multiline = true;
+            this.txtDataTask.Name = "txtDataTask";
+            this.txtDataTask.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDataTask.Size = new System.Drawing.Size(355, 82);
+            this.txtDataTask.TabIndex = 7;
+            // 
+            // txtQuiz
+            // 
+            this.txtQuiz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuiz.Location = new System.Drawing.Point(15, 283);
+            this.txtQuiz.Multiline = true;
+            this.txtQuiz.Name = "txtQuiz";
+            this.txtQuiz.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtQuiz.Size = new System.Drawing.Size(794, 182);
+            this.txtQuiz.TabIndex = 8;
+            // 
+            // btnQuiz
+            // 
+            this.btnQuiz.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuiz.Location = new System.Drawing.Point(711, 242);
+            this.btnQuiz.Name = "btnQuiz";
+            this.btnQuiz.Size = new System.Drawing.Size(98, 35);
+            this.btnQuiz.TabIndex = 9;
+            this.btnQuiz.Text = "Quiz";
+            this.btnQuiz.UseVisualStyleBackColor = true;
+            this.btnQuiz.Click += new System.EventHandler(this.btnQuiz_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 477);
+            this.Controls.Add(this.btnQuiz);
+            this.Controls.Add(this.txtQuiz);
+            this.Controls.Add(this.txtDataTask);
+            this.Controls.Add(this.txtDataStudent);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAddStu);
+            this.Controls.Add(this.txtStuName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtStuName;
+        private System.Windows.Forms.Button btnAddStu;
+        private System.Windows.Forms.Button btnAddTask;
+        public System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox txtDataStudent;
+        public System.Windows.Forms.TextBox txtDataTask;
+        public System.Windows.Forms.TextBox txtQuiz;
+        private System.Windows.Forms.Button btnQuiz;
+    }
+}
+
---
+++ QuizApp/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizApp
+{
+    public partial class Form1 : Form
+    {
+        public List<string> students = new List<string>();
+        public List<string> tasks = new List<string>();
+        public Random random = new Random();
+        int count = 1;
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void btnAddStu_Click(object sender, EventArgs e)
+        {
+            string name = txtStuName.Text;
+            students.Add(name);
+            Read();
+            CLeaner();
+        }
+
+        public void CLeaner()
+        {
+            txtStuName.Clear();
+            txtTask.Clear();
+        }
+
+        public void Read()
+        {
+            txtDataStudent.Text = "";
+            txtDataTask.Text = "";
+            int stuCount = 1;
+            int taskCount = 1;
+            foreach (string student in students)
+            {
+                txtDataStudent.Text += stuCount + " - "+ student + "\r\n";
+                stuCount++;
+            }
+            foreach (string task in tasks)
+            {
+                txtDataTask.Text += taskCount + " - " + task + "\r\n";
+                taskCount++;
+            }
+
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            string task = txtTask.Text;
+            tasks.Add(task);
+            Read();
+            CLeaner();
+        }
+
+        private void btnQuiz_Click(object sender, EventArgs e)
+        {
+            int stu_index;
+            int task_index;
+            do
+            {
+                if (students.Count == 1)
+                {
+                    stu_index = 0;
+                    task_index = 0;
+                    btnQuiz.Enabled = false;
+                    break;
+                }
+                stu_index = random.Next(students.Count);
+                task_index = random.Next(tasks.Count); 
+            } while (stu_index == task_index); 
+            txtQuiz.Text += count + " Student: " + students[stu_index] + " Task: " + tasks[task_index] + "\r\n";
+            students.RemoveAt(stu_index); 
+            tasks.RemoveAt(task_index);
+            Read();
+            count++;
+        }
+    }
+}
